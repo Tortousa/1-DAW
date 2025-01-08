1e1669cf90d4c6f35870ec656f980b86c1e943bd
Daniel Tortosa <151463782+Tortousa@users.noreply.github.com>
2025-01-09 00:18:45 +0100

    Update Main.java

--- U4/Ejercicio3/src/Libros/Main.java
+++ U4/Ejercicio3/src/Libros/Main.java
@@ -1,5 +1,5 @@
-package src.Libros;
-
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -7,21 +7,25 @@ public class Main {
     public static final int CANTIDAD_LIBROS = 3;
 
     public static void main(String[] args) {
+    	Scanner patata = new Scanner(System.in);
+    	
         Libro[] libros = new Libro[CANTIDAD_LIBROS];
 
         for(int i = 0; i < libros.length; i++) {
             System.out.println("Libro " + (i + 1));
-            libros[i] = pedirLibro();
+            libros[i] = pedirLibro(patata);
             System.out.println();
         }
+        
+        Libro[] librosOrd = ordenarLibro(libros);
 
-        Menu(libros);
+        crearMenu(librosOrd, patata);
+        
+        patata.close();
 
     }
 
-    public static Libro pedirLibro() {
-        Scanner patata = new Scanner(System.in);
-
+    public static Libro pedirLibro(Scanner patata) {
         Libro libros = new Libro();
 
         System.out.print("Titulo: ");
@@ -35,32 +39,55 @@ public class Main {
     }
     
     public static Libro[] ordenarLibro(Libro[] libros) {
-		for(int i = 0; i < libros.length; i++) {
-			libros[i].getTitulo();
-		}
+    	Comparator<Libro> comparador = (libro1,libro2) -> (libro1).getTitulo().compareTo((libro2).getTitulo());
+    	//Compara dos objetos, libro1 y libro2. No son libro[0] y libro[1] sino que compara dos objetos a lo largo del proceso.
+        Arrays.sort(libros, comparador);
+        
         return libros;
     }
     
-    public static void Menu(Libro[] libros) {
-		Scanner patata = new Scanner(System.in);
-
-        int opcion;
-
+    public static void crearMenu(Libro[] libros, Scanner patata) {
 		System.out.println("--- Titulos ---");
+		
+		int opcion;
 
 		for(int i = 0; i < libros.length; i++) {
 			System.out.println((i + 1) + ". " + libros[i].getTitulo());
 		}
-
-		System.out.print("Opcion: ");
-		opcion = patata.nextInt();
-
-		if(opcion > 0 && opcion <= libros.length) {
-			mostrarLibro(libros, opcion - 1);
-		} else {
-			System.out.println("Hasta luego!");
-		}
+		
+		System.out.println("0. Salir");
+		
+		do {
+			opcion = pedirInt("Opcion: ", patata);
+			
+			if(opcion > 0 && opcion <= libros.length) {
+				mostrarLibro(libros, opcion - 1);
+				break;
+			} if(opcion < 0 || opcion > libros.length) {
+				System.out.println("Opcion no valida.");
+				continue;
+			} else {
+				System.out.println("Hasta luego!");
+			}
+		}while(opcion != 0);
     }
+    
+    public static int pedirInt(String enunciado, Scanner patata) {
+		int n = 0;
+		boolean check = false;
+
+		do {
+			System.out.print(enunciado);
+
+			try {
+				n = Integer.parseInt(patata.nextLine().trim());
+				check = true;
+			} catch (Exception e) {
+				System.out.println("Error, no has introducido un numero.");
+			}
+		} while (!check);
+		return n;
+	}
 
     public static void mostrarLibro(Libro[] libros, int indice) {
 		Libro libro = libros[indice];
@@ -69,5 +96,4 @@ public class Main {
 		System.out.println("Autor: " + libro.getAutor());
 		System.out.println("ISBN: " + libro.getIsbn());
 	}
-	
 }
