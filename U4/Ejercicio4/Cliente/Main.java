a0c713f4ec05c75dbe139a4e2f34e716897e2d7c
Daniel Tortosa <151463782+Tortousa@users.noreply.github.com>
2025-01-14 16:56:46 +0100

    Add files via upload

---
+++ U4/Ejercicio4/Cliente/Main.java
@@ -0,0 +1,26 @@
+package com.clases.clase05;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente[] listaClientes = {new Cliente("Ana", "Lopez"), 
+				new Cliente("Paco", "Martinez"), 
+				new Cliente("Pedro", "Fernandez"), 
+				new Cliente("Maria", "Alvarez")
+		};
+	
+		System.out.println("---- Lista Desordenada ----");
+		
+		for(Cliente c: listaClientes) {
+			System.out.println(c);
+		}
+		
+		OrdenarClientes.ordenarClientes(listaClientes);
+		
+		System.out.println("---- Lista Ordenada ----");
+		
+		for(Cliente c: listaClientes) {
+			System.out.println(c);
+		}
+	}
+}
---
+++ U4/Ejercicio4/Cliente/OrdenarClientes.java
@@ -0,0 +1,21 @@
+package com.clases.clase05;
+
+public class OrdenarClientes {
+	
+	public static Cliente[] ordenarClientes(Cliente[] array) {
+		if(array == null || array.length < 2) {
+			return array;
+		}
+		
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array.length - i - 1; j++) {
+				if(array[j].getApellidos().compareToIgnoreCase(array[j + 1].getApellidos()) > 0) {
+					Cliente aux = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = aux;
+				}
+			}
+		}
+		return array;
+	}
+}
