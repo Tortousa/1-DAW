85c351f1b506d25c08a296fa7fae1003b19569d8
Daniel Tortosa <151463782+Tortousa@users.noreply.github.com>
2025-01-28 18:01:11 +0100

    Add files via upload

---
+++ U5/ProyectosLibrosRA/src/com/ejemplo/libreria/Libro.java
@@ -0,0 +1,74 @@
+package com.ejemplo.libreria;
+
+public class Libro {
+	private String titulo, autor, editorial, isbn;
+	private int anioPublicacion;
+	private double precio;
+	
+	public Libro(String titulo, String autor, String editorial, String isbn, int anioPublicacion, double precio) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editorial = editorial;
+		this.isbn = isbn;
+		this.anioPublicacion = anioPublicacion;
+		this.precio = precio;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getAnioPublicacion() {
+		return anioPublicacion;
+	}
+
+	public void setAnioPublicacion(int anioPublicacion) {
+		this.anioPublicacion = anioPublicacion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Título: " + titulo +
+	           "\nAutor: " + autor +
+	           "\nEditorial: " + editorial +
+	           "\nIsbn: " + isbn +
+	           "\nAño: " + anioPublicacion +
+	           "\nPrecio: " + precio + " €";
+	}
+}
---
+++ U5/ProyectosLibrosRA/src/com/ejemplo/libreria/Main.java
@@ -0,0 +1,77 @@
+package com.ejemplo.libreria;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String rutaArchivo = "./files/libros.txt";
+		
+		ArrayList<Libro> libros = importarLibros(rutaArchivo);
+
+		mostrarMenu(libros, sc);
+		
+	}
+	
+	public static void mostrarMenu(ArrayList<Libro> libros, Scanner sc) {
+		String opcion = "";
+		System.out.println("Titulos de los libros\n-------------------");
+		
+	    for (int i = 0; i < libros.size(); i++) {
+	        System.out.println((i + 1) + " - " + libros.get(i).getTitulo());
+	    }
+
+	    System.out.print("Titulo: ");
+	    opcion = sc.nextLine().trim();
+
+	    boolean check = false;
+
+	    for (Libro i : libros) {
+	        if (opcion.equalsIgnoreCase(i.getTitulo())) {
+	            mostrarLibro(i);
+	            
+	        }
+	    }
+
+	    if (!check) {
+	        System.out.println("Título no válido.");
+	    }
+	}
+
+	public static void mostrarLibro(Libro libro) {
+	    System.out.println(libro);
+	    System.out.println();
+	}
+	
+	public static ArrayList<Libro> importarLibros(String rutaArchivo) {
+		ArrayList<Libro> librosFuncion = new ArrayList<>();
+		
+		File archivo = new File(rutaArchivo);
+		
+		try(Scanner lector = new Scanner(archivo)){
+			while(lector.hasNextLine()) {
+				String linea = lector.nextLine();
+				String[] datos = linea.split(",,");
+				
+				if(datos.length == 6) {
+					try {
+						int anio = Integer.parseInt(datos[4]);
+						double precio = Double.parseDouble(datos[5]);
+						
+						Libro libro = new Libro(datos[0], datos[1], datos[2], datos[3], anio, precio);
+						
+						librosFuncion.add(libro);
+						
+					} catch(NumberFormatException e) {
+						System.out.println("Error en la linea " + linea);
+					}
+				}
+			}
+		}catch(Exception e) {
+			System.out.println("Error, no se encontro el archivo: " + rutaArchivo);
+		}
+		return librosFuncion;
+	}
+}
