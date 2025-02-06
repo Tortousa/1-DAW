372057d57eee1221cef0ea788bed61e023e63f95
Daniel Tortosa <151463782+Tortousa@users.noreply.github.com>
2025-02-06 02:05:57 +0100

    Update Main.java

--- U5/ProyectosLibrosRA/src/com/ejemplo/libreria/Main.java
+++ U5/ProyectosLibrosRA/src/com/ejemplo/libreria/Main.java
@@ -6,54 +6,60 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class Main {
+	
+	public static final String MENU = "1 - Mostrar Libros\n2 - Buscar libro por titulo\n3 - Salir";
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner patata = new Scanner(System.in);
 		String rutaArchivo = "./files/libros.txt";
 		
 		ArrayList<Libro> libros = importarLibros(rutaArchivo);
 
-		mostrarMenu(libros, sc);
+		mostrarMenu(libros, patata);
 		
 	}
 	
-	public static void mostrarMenu(ArrayList<Libro> libros, Scanner sc) {
-		String opcion = "";
-		System.out.println("Titulos de los libros\n--------------------");
+	public static void mostrarMenu(ArrayList<Libro> libros, Scanner patata) {
+		String titulo = "";
+		int opcion;
 		
-	    for (int i = 0; i < libros.size(); i++) {
-	        System.out.println((i + 1) + " - " + libros.get(i).getTitulo());
-	    }
-
-	    System.out.print("Titulo: ");
-	    opcion = sc.nextLine().trim();
-
-	    if(validarTitulo(opcion)) {
-	    	System.out.println("Titulo encontrado.\n");
-	    	
-	    	for (Libro i : libros) {
-	    		if (opcion.equalsIgnoreCase(i.getTitulo())) {
-	    			mostrarLibro(i);
-	    			return;
+		do {
+			System.out.println(MENU);
+			opcion = pedirInt("Opcion nº: ", patata);
+			
+			if(opcion < 0 || opcion > 3) {
+				System.out.println("Opcion no valida.");
+			}
+		}while(opcion < 0 || opcion > 3);
+		
+		if(opcion == 1) {
+			System.out.println("Libros almacenados\n--------------------");
+			
+			mostrarLibro(libros);
+		}
+		if(opcion == 2) {
+			for (int i = 0; i < libros.size(); i++) {
+		        System.out.println((i + 1) + " - " + libros.get(i).getTitulo());
+		    }
+		    
+			System.out.print("Titulo: ");
+			titulo = patata.nextLine().trim();
+			
+			for (Libro i : libros) {
+	    		if (titulo.equalsIgnoreCase(i.getTitulo())) {
+	    			 mostrarLibro(libros);
+	    			 return;
 	        	}
 	    	}
-	    } else {
-	    	System.out.println("Titulo no encontrado.");
-	    }
-
-	    if (!validarTitulo(opcion)) {
-	        System.out.println("Título no valido.");
-	    } else {
-	    	System.out.println("Título valido.");
-	    }
-	}
-
-	public static void mostrarLibro(Libro libro) {
-	    System.out.println(libro);
-	    System.out.println();
+		}
+		if(opcion == 3) {
+			System.out.println("Hasta luego!");
+		}
 	}
 	
-	public static boolean validarTitulo(String titulo) {
-		return Libro.regexTitulo.matcher(titulo).matches();
+	public static void mostrarLibro(ArrayList<Libro> libros) {
+	    System.out.println(libros);
+	    System.out.println();
 	}
 	
 	public static ArrayList<Libro> importarLibros(String rutaArchivo) {
@@ -84,5 +90,22 @@ public class Main {
 			System.out.println("Error, no se encontro el archivo: " + rutaArchivo);
 		}
 		return librosFuncion;
+	}
+	
+	public static int pedirInt(String enunciado, Scanner patata) {
+		int n = 0;
+		boolean check = false;
+
+		do {
+			System.out.print(enunciado);
+
+			try {
+				n = Integer.parseInt(patata.nextLine().trim());
+				check = true;
+			} catch (Exception e) {
+				System.out.println("Error, no has introducido un numero.");
+			}
+		} while (!check);
+		return n;
 	}
 }
